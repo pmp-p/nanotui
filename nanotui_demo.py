2f33cb7b225dabc0ea7d5811e67d2a8cab807b82
pmp-p <pmp-p@users.noreply.github.com>
2017-06-18 01:10:19 +0200

    Add files via upload

--- nanotui_demo.py
+++ nanotui_demo.py
@@ -5,10 +5,6 @@ import nanotui.events as events
 import time
 
 
-def zfill(i,places,char='0'):
-    i=str(i)
-    if len(i)<places:  i = '%s%s' % (  char * ( places-len(i) ) , i )
-    return i
 
 with nanotui.VisualPage( 'demo' ,"nanotui demo",80, 25) as add:
     try:
@@ -17,6 +13,7 @@ with nanotui.VisualPage( 'demo' ,"nanotui demo",80, 25) as add:
             clk = '%s%c%s%c%s'  % ( zfill(h,2),':',zfill(m,2),':',zfill(s,2) )
             Time.sleep(.1)
             status(clk)
+            choice.set_text( '%-4s : "%s"' % ( str(lb.choice),lb.get_text() ) )
 
 
 
@@ -24,7 +21,13 @@ with nanotui.VisualPage( 'demo' ,"nanotui demo",80, 25) as add:
 
         status =add.anchor(" ----------- Label -------------",dy=-3)
 
-        last= add('units','ListBox',items="1 2 3 4".split(' '),width=51,height=6,x=3,y=last.y+last.height+4 )
+
+        last=add(None,'Label',text='Size:',x=4)
+        p1 = add('p1','InputField',text='4096',y=last.y-1) #,x=4)
+
+        choice =add('choice','Label',text=" selection : %s",x=2)#,y=status.y+2 )
+
+        last= lb =  add('units','ListBox',items="1 2 3 4".split(' '),width=51,height=6,x=3,y=last.y+last.height+4 )
         add(None,'Frame',text="Listbox Frame :",x=last.x-2,y=last.y-2,width=last.width+2,height=last.height+4)
 
 
